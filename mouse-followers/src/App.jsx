7894a4b48df9d42f6fb62d4d9eb6428af085b75a
Jonathan <jonathan.ibanez-pinero@iesruizgijon.com>
2026-04-17 14:45:04 +0200

    comienzo

--- mouse-followers/src/App.jsx
+++ mouse-followers/src/App.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
   useEffect(() => {
-
-  })
+    console.log('Efecto', { enabled })
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
   return (
     <>
       <h3>Proyecto de midudev 3</h3>
-      <button>Activar Seguir puntero</button>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Activar" : "Desactivar"}
+        Seguir puntero</button>
     </>
   )
 }
